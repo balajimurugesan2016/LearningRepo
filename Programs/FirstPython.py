98f4e76b2e2ca3a1a8cdeac4a5565bd0a3856a35
balaji <balajimurugesan.2016@gmail.com>
2017-11-12 04:48:26 +0530

    First Machine learning program

--- Programs/FirstPython.py
+++ Programs/FirstPython.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plot
 import pandas as pd
 
-#changes#
-
-
-
+dataset = pd.read_csv('Data.csv')
+independent = dataset.iloc[:, :-1].values
+print(independent)
+dependent = dataset.iloc[:, 3].values
+print(dependent)
